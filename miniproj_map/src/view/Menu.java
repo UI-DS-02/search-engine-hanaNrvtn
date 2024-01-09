24df35156fc6499b58f3bbec191a126143350212
hanaNrvtn <hanaNrvtn@gmail.com>
2024-01-09 11:25:39 -0800

    Menu.java and View.java as view package classes.

---
+++ miniproj_map/src/view/Menu.java
@@ -0,0 +1,13 @@
+package view;
+
+public class Menu {
+
+    public void printMenu() {
+
+        System.out.println(
+                """
+                        1. basic search\s
+                        2. prioritised search
+                        3. exit""");
+    }
+}
---
+++ miniproj_map/src/view/View.java
@@ -0,0 +1,127 @@
+package view;
+
+import Controller.DataHandler;
+import Controller.Editor;
+import Controller.Mapper;
+import Controller.SearchHandler;
+import model.FinDataFile;
+import model.Result;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Map;
+import java.util.Set;
+
+public class View {
+
+    // field
+    private final Editor edt;
+    private final Mapper mpr;
+    private final DataHandler dh;
+    private final SearchHandler sh;
+
+    private final String src; // dir
+    private final String dst; // dir
+    private final String sw;  // file
+    private final String rgx;
+
+
+    // getter
+    public Mapper getMpr() {
+        return mpr;
+    }
+
+    public DataHandler getDh() {
+        return dh;
+    }
+
+    public SearchHandler getSh() {
+        return sh;
+    }
+
+    public String getSrc() {
+        return src;
+    }
+
+    public String getDst() {
+        return dst;
+    }
+
+    public String getSw() {
+        return sw;
+    }
+
+    public String getRgx() {
+        return rgx;
+    }
+
+
+    // cons
+    public View(int swh) throws IOException {
+
+        src = "file\\in";
+        dst = "file\\out";
+        sw = "stopwords.txt";
+        rgx = "[^a-z]+";
+
+        edt = new Editor(rgx, sw);
+        mpr = new Mapper();
+        dh = new DataHandler(src, dst, edt, mpr);
+        sh = new SearchHandler(mpr);
+
+        init(swh);
+    }
+
+
+    // method
+    public void init(int swh)
+            throws IOException {
+
+        dh.edit(swh);
+        dh.map();
+    }
+
+    public void bscSearch(String s)
+            throws IOException {
+
+        //init(1);
+
+        Set<FinDataFile> res = sh.bscSearch(s);
+
+        // out
+        System.out.println(res.size());
+        for (FinDataFile f : res) {
+            System.out.println(f.getName());
+        }
+
+    }
+
+    public void prtSearch(String s)
+            throws IOException {
+
+        //init(2);
+
+        ArrayList<Result> res = sh.prtSearch(s, edt);
+
+        // out
+        System.out.println(res.size());
+        /*
+        StringBuilder sb = new StringBuilder();
+        for (Result r : res) {
+            sb.append(r.getFdf().getName()).append(" ess: ");
+            for (Map.Entry<String, Integer> e : r.getEss().entrySet()) {
+                sb.append(e.getKey()).append(": ").append(e.getValue()).append("|");
+            }
+            System.out.println("\n");
+            for (Map.Entry<String, Integer> e : r.getOpt().entrySet()) {
+                sb.append(e.getKey()).append(": ").append(e.getValue()).append("|");
+            }
+            sb.append("\n\n");
+        }
+        System.out.println(sb);
+         */
+        for (int i=0;i<res.size();i++) {
+            System.out.println(res.get(i).toString());
+        }
+    }
+}
